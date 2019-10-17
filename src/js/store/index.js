9e1df802d1b0773da7008ccbef4a6b772efcf22e
InesTlili <inestlili2112@gmail.com>
2019-10-17 22:39:38 +0200

    persist state json

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,5 +1,33 @@
 
+
+
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
-const store = createStore(rootReducer);
+
+// load function for the state
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (!serializedState) return undefined;
+    else return JSON.parse(serializedState);
+  } catch(err) {
+    return undefined;
+  }
+};
+
+
+//to persist the state we save it in a file
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch(err) {
+    console.log(err);
+  }
+};
+const persistedStore = loadState();
+const store = createStore(rootReducer, persistedStore);
+store.subscribe(() => {
+    saveState(store.getState());
+  });
 export default store;
